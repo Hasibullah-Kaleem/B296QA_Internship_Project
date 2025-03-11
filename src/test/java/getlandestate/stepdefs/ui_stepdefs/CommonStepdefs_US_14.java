58891020e55844df7daf468c58c34c9e7876023e
Goychak M <musayevagoychak2@gmail.com>
2025-03-11 11:18:01 +0400

    version 7

---
+++ src/test/java/getlandestate/stepdefs/ui_stepdefs/CommonStepdefs_US_14.java
@@ -0,0 +1,37 @@
+package getlandestate.stepdefs.ui_stepdefs;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class CommonStepdefs_US_14 {
+    @Given("User navigates to {string} using a web browser")
+    public void userNavigatesToUsingAWebBrowser(String arg0) {
+    }
+
+    @When("User clicks on the Login button on the homepage")
+    public void userClicksOnTheLoginButtonOnTheHomepage() {
+    }
+
+    @And("User enters valid manager credentials with {string} and {string} in the login form")
+    public void userEntersValidManagerCredentialsWithAndInTheLoginForm(String arg0, String arg1) {
+    }
+
+    @And("User clicks on the Login button at the top-right corner of the website to authenticate")
+    public void userClicksOnTheLoginButtonAtTheTopRightCornerOfTheWebsiteToAuthenticate() {
+    }
+
+    @And("User clicks on the Back to Site tab located on the left side of the dashboard")
+    public void userClicksOnTheBackToSiteTabLocatedOnTheLeftSideOfTheDashboard() {
+    }
+
+    @And("User clicks on the Properties tab at the top of the website")
+    public void userClicksOnThePropertiesTabAtTheTopOfTheWebsite() {
+    }
+
+    @And("User selects the first house displayed on the page and clicks on it")
+    public void userSelectsTheFirstHouseDisplayedOnThePageAndClicksOnIt() {
+    }
+
+}
--- src/test/java/getlandestate/stepdefs/ui_stepdefs/Stepdefs_US_14.java
+++ src/test/java/getlandestate/stepdefs/ui_stepdefs/Stepdefs_US_14.java
@@ -1,4 +1,26 @@
 package getlandestate.stepdefs.ui_stepdefs;
 
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+
 public class Stepdefs_US_14 {
+    @And("User schedules a tour by selecting a Tour Date and Tour Time from the options on the right side of the page")
+    public void userSchedulesATourBySelectingATourDateAndTourTimeFromTheOptionsOnTheRightSideOfThePage() {
+    }
+
+    @And("User confirms the selected date by clicking on the Submit a Tour Request button")
+    public void userConfirmsTheSelectedDateByClickingOnTheSubmitATourRequestButton() {
+    }
+
+    @Then("User should see the notification Tour request created successfully appear on the page")
+    public void userShouldSeeTheNotificationTourRequestCreatedSuccessfullyAppearOnThePage() {
+    }
+
+    @And("User clicks on the Submit a Tour Request button without selecting a Tour Date or Tour Time")
+    public void userClicksOnTheSubmitATourRequestButtonWithoutSelectingATourDateOrTourTime() {
+    }
+
+    @Then("User should see an error message asking them to select both a Tour Date and Tour Time before submitting the request")
+    public void userShouldSeeAnErrorMessageAskingThemToSelectBothATourDateAndTourTimeBeforeSubmittingTheRequest() {
+    }
 }
