b4d25a7d11bda2fc7a5f13a093143fd4850cbe96
HulyaTascioglu <tasciogluhulyaa@gmail.com>
2025-03-19 20:34:30 +0300

    Temporary commit: changes in Runner.java and test-output files

--- src/test/java/getlandestate/runners/Runner.java
+++ src/test/java/getlandestate/runners/Runner.java
@@ -22,7 +22,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features/ui_features",
         glue = {"getlandestate.stepdefs.ui_stepdefs", "getlandestate.hooks"},
         dryRun = false,
-        tags =  "@us_16"
+        tags =  "@us_15"
 
 )
 public class Runner {
