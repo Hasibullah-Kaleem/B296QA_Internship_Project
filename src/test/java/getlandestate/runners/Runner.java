3e217589442587c17f3ca23c60f4a4504971e116
HulyaTascioglu <tasciogluhulyaa@gmail.com>
2025-03-25 23:16:28 +0300

    Add DB_US_14 feature file and step definitions

--- src/test/java/getlandestate/runners/Runner.java
+++ src/test/java/getlandestate/runners/Runner.java
@@ -23,7 +23,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = {"getlandestate.stepdefs", "getlandestate.hooks"},
         dryRun = false,
-        tags =  "@db_us_18"
+        tags =  "@db_us_14"
 
 
 )
