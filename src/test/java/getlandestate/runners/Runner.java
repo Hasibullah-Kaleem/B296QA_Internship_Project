3ff2aa3263c198ad5daad4c22f6a5a5a85939a7b
Goychak M <musayevagoychak2@gmail.com>
2025-03-27 00:44:20 +0400

    api_version_02

--- src/test/java/getlandestate/runners/Runner.java
+++ src/test/java/getlandestate/runners/Runner.java
@@ -23,7 +23,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = {"getlandestate.stepdefs", "getlandestate.hooks"},
         dryRun = false,
-        tags =  "@api_us_02_tc_01"
+        tags =  "@api_us_02_tc_02"
 
 
 )
